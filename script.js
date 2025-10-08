105bcf7686575762763a53ee2de39d97154ee45f
shinkensen <noobywinner587@gmail.com>
2025-10-07 22:24:58 -0500

    finished page 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const waffle =document.getElementById("waffle");
 const title=document.getElementById("title");
-const colors = ["red", "blue" ,"green", "white"];
+const colors = ["red", "blue" ,"green", "white","burlywood"];
 let color=-1;
 waffle.style.transition= 'transform 0.2s ease-in-out'
 let rot=10;
@@ -11,7 +11,7 @@ waffle.addEventListener("mousemove",()=>{
     }
 })
 waffle.addEventListener("click",()=>{
-    if (color<3){
+    if (color<4){
         color++;
     }
     else{
@@ -20,8 +20,21 @@ waffle.addEventListener("click",()=>{
     waffle.style.borderColor = colors[color];
 })
 document.addEventListener("mousemove", (event)=>{
-    const titleBox= title.clientLeft;
+    const titleWidth= title.getBoundingClientRect().width /2;
+    const titleHeight= title.getBoundingClientRect().height /2;
     const x= event.clientX;
     const y= event.clientY;
-
+    const angleX = Math.min(1, ((x-titleWidth) / titleWidth)) *-45;
+    const angleY = ((y-titleHeight) / titleHeight) *2;
+    title.style.transform = `rotateX(${angleY}deg)`
+    title.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg)`
+})
+title.addEventListener("click", () =>{
+    if (color<4){
+        color++;
+    }
+    else{
+        color=0;
+    }
+    title.style.color = colors[color];
 })
