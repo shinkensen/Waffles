f2f170ebf0eb4d29ff07f6520e3622c6106f2112
shinkensen <noobywinner587@gmail.com>
2025-10-08 17:46:54 -0500

    finished page 2

--- script.js
+++ script.js
@@ -1,5 +1,64 @@
 const waffle =document.getElementById("waffle");
 const title=document.getElementById("title");
+const class1=document.getElementsByClassName("animation");
+const left= document.getElementById("left");
+const right= document.getElementById("right");
+const desc= document.getElementById("desc");
+const img= document.getElementById("img");
+
+const imgs = ["images/waffle1.png","images/waffle2.png","images/waffle3.png","images/waffle4.png"];
+const descs = ["A tasty buttered up waffle" , "A tastier sugar waffle" , "A triangular waffle", "A Rectangular waffle"]
+let image=0;
+left.addEventListener("click",() => {
+    if (image==0){
+        image=3;
+    }
+    else{
+        image--;
+    }
+    imgChanger();
+}
+)
+
+right.addEventListener("click",() => {
+    if (image==3){
+        image=0;
+    }
+    else{
+        image++;
+    }
+    imgChanger();
+}
+)
+
+function imgChanger(){
+    img.src = imgs[image];
+    desc.textContent = descs[image];
+    desc.style.color = "bisque";
+}
+
+let num=0;
+setInterval(()=>{
+    for (let title2 of class1){
+        title2.style.borderColor = "black";
+        if (num>3){
+            num=0;
+        }
+        if (num==0){
+            title2.style.borderBottom = "1px solid bisque";
+        }
+        else if (num==1){
+            title2.style.borderRight = "1px solid bisque";
+        }
+        else if (num==2){
+            title2.style.borderTop = "1px solid bisque";
+        }
+        else if (num==3){
+            title2.style.borderLeft = "1px solid bisque";
+        }
+    num++;
+}
+},100)
 const colors = ["red", "blue" ,"green", "white","burlywood"];
 let color=-1;
 waffle.style.transition= 'transform 0.2s ease-in-out'
