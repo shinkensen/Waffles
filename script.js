4d79a8a17f210f82fbd7fd029f69221e36e6e0c3
shinkensen <noobywinner587@gmail.com>
2025-10-07 21:36:52 -0500

    stuff

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-let left=true;
-setInterval(() => {
-    let image= Math.random()*2 +1 %2 ==0? "images/strawberry.png" : "images/whipcream.png";
-    const img=document.createElement("img",{src:image});
-}, 5000);   
+const waffle =document.getElementById("waffle");
+const title=document.getElementById("title");
+const colors = ["red", "blue" ,"green", "white"];
+let color=-1;
+waffle.style.transition= 'transform 0.2s ease-in-out'
+let rot=10;
+waffle.addEventListener("mousemove",()=>{
+    waffle.style.transform = `rotate(${rot}deg)`;
+    if (rot<43945935903094){
+        rot+=10;
+    }
+})
+waffle.addEventListener("click",()=>{
+    if (color<3){
+        color++;
+    }
+    else{
+        color=0;
+    }
+    waffle.style.borderColor = colors[color];
+})
+document.addEventListener("mousemove", (event)=>{
+    const titleBox= title.clientLeft;
+    const x= event.clientX;
+    const y= event.clientY;
+
+})
